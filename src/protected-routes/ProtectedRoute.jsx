62e4a7da7617a59edad41de61da5273b00a236ee
the-pro7 <ameyawemmanuel900@gmail.com>
2023-09-28 19:53:31 +0100

    Update ProtectedRoute.jsx

--- src/protected-routes/ProtectedRoute.jsx
+++ src/protected-routes/ProtectedRoute.jsx
@@ -1,4 +1,4 @@
-import { Navigate, Route, redirect } from 'react-router-dom'
+import { Navigate} from 'react-router-dom'
 import { useAuth } from '../authContext'
 
 const ProtectedRoute = ({children}) => {
